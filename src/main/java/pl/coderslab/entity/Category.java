921f54e9133e2ce3d4ed3390339218c944d2b2ff
Przemysław Konik <przemyslawkonik@gmail.com>
2017-12-20 21:10:50 +0100

    create entity Category

---
+++ src/main/java/pl/coderslab/entity/Category.java
@@ -0,0 +1,84 @@
+package pl.coderslab.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "category")
+public class Category {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@Column(length = 100)
+	private String name;
+
+	@Column(nullable = true)
+	private String description;
+
+	@ManyToMany(cascade = { CascadeType.MERGE, CascadeType.PERSIST })
+	@JoinTable(joinColumns = { @JoinColumn(name = "article_id") }, inverseJoinColumns = {
+			@JoinColumn(name = "category_id") })
+	private List<Article> articles;
+
+	public Category() {
+		articles = new ArrayList<>();
+	}
+
+	public Category(String name, String description, List<Article> articles) {
+		this.name = name;
+		this.description = description;
+		this.articles = articles;
+	}
+
+	public Category(int id, String name, String description, List<Article> articles) {
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.articles = articles;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public List<Article> getArticles() {
+		return articles;
+	}
+
+	public void setArticles(List<Article> articles) {
+		this.articles = articles;
+	}
+
+}
