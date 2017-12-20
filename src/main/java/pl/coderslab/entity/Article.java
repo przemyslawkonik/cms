51d60ed656c1f794b6280eb4ccff8bfeb7d3b77c
Przemysław Konik <przemyslawkonik@gmail.com>
2017-12-20 21:10:34 +0100

    create entity Article

---
+++ src/main/java/pl/coderslab/entity/Article.java
@@ -0,0 +1,126 @@
+package pl.coderslab.entity;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+@Table(name = "article")
+public class Article {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@Column(length = 200)
+	private String title;
+
+	@OneToOne
+	private Author author;
+
+	@ManyToMany(fetch = FetchType.EAGER, mappedBy = "articles", cascade = { CascadeType.MERGE, CascadeType.PERSIST })
+	private List<Category> categories;
+
+	private String content;
+
+	@CreationTimestamp
+	private LocalDateTime created;
+
+	@UpdateTimestamp
+	private LocalDateTime updated;
+
+	public Article() {
+		categories = new ArrayList<>();
+	}
+
+	public Article(String title, Author author, List<Category> categories, String content, LocalDateTime created,
+			LocalDateTime updated) {
+		this.title = title;
+		this.author = author;
+		this.categories = categories;
+		this.content = content;
+		this.created = created;
+		this.updated = updated;
+	}
+
+	public Article(int id, String title, Author author, List<Category> categories, String content,
+			LocalDateTime created, LocalDateTime updated) {
+		this.id = id;
+		this.title = title;
+		this.author = author;
+		this.categories = categories;
+		this.content = content;
+		this.created = created;
+		this.updated = updated;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Author getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(Author author) {
+		this.author = author;
+	}
+
+	public List<Category> getCategories() {
+		return categories;
+	}
+
+	public void setCategories(List<Category> categories) {
+		this.categories = categories;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public LocalDateTime getCreated() {
+		return created;
+	}
+
+	public void setCreated(LocalDateTime created) {
+		this.created = created;
+	}
+
+	public LocalDateTime getUpdated() {
+		return updated;
+	}
+
+	public void setUpdated(LocalDateTime updated) {
+		this.updated = updated;
+	}
+
+}
