2807af9dbdd5acb3f0a1382585ad94b72f5767ba
Przemysław Konik <przemyslawkonik@gmail.com>
2017-12-21 20:02:34 +0100

    create and implement AuthorController

---
+++ src/main/java/pl/coderslab/controller/AuthorController.java
@@ -0,0 +1,62 @@
+package pl.coderslab.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import pl.coderslab.dao.AuthorDao;
+import pl.coderslab.entity.Author;
+import pl.coderslab.entity.Category;
+
+@Controller
+@RequestMapping("/authors")
+public class AuthorController {
+	private AuthorDao authorDao;
+
+	@Autowired
+	public AuthorController(AuthorDao authorDao) {
+		this.authorDao = authorDao;
+	}
+
+	@GetMapping("")
+	public String show(Model m) {
+		m.addAttribute("authors", authorDao.findAll());
+		return "/author/authors";
+	}
+
+	@GetMapping("/add")
+	public String add(Model m) {
+		m.addAttribute("author", new Category());
+		return "/author/addAuthor";
+	}
+
+	@PostMapping("/add")
+	public String add(@ModelAttribute Author author) {
+		authorDao.save(author);
+		return "redirect:/authors";
+	}
+
+	@GetMapping("/delete/{id}")
+	public String delete(@PathVariable int id) {
+		authorDao.remove(id);
+		return "redirect:/authors";
+	}
+
+	@GetMapping("/edit/{id}")
+	public String edit(Model m, @PathVariable int id) {
+		m.addAttribute("author", authorDao.findById(id));
+		return "/author/addAuthor";
+	}
+
+	@PostMapping("/edit/{id}")
+	public String edit(@ModelAttribute Author author) {
+		authorDao.update(author);
+		return "redirect:/authors";
+	}
+
+}
