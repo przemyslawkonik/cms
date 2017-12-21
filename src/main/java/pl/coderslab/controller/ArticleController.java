f4f64a6351a80b980421822977cf1a9d2228348e
Przemysław Konik <przemyslawkonik@gmail.com>
2017-12-21 20:11:32 +0100

    create ArticleController

---
+++ src/main/java/pl/coderslab/controller/ArticleController.java
@@ -0,0 +1,61 @@
+package pl.coderslab.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import pl.coderslab.dao.ArticleDao;
+import pl.coderslab.entity.Article;
+
+@Controller
+@RequestMapping("/articles")
+public class ArticleController {
+	private ArticleDao articleDao;
+
+	@Autowired
+	public ArticleController(ArticleDao articleDao) {
+		this.articleDao = articleDao;
+	}
+
+	@GetMapping("")
+	public String show(Model m) {
+		m.addAttribute("articles", articleDao.findAll());
+		return "/article/articles";
+	}
+
+	@GetMapping("/add")
+	public String add(Model m) {
+		m.addAttribute("article", new Article());
+		return "/article/addArticle";
+	}
+
+	@PostMapping("/add")
+	public String add(@ModelAttribute Article article) {
+		articleDao.save(article);
+		return "redirect:/articles";
+	}
+
+	@GetMapping("/delete/{id}")
+	public String delete(@PathVariable int id) {
+		articleDao.remove(id);
+		return "redirect:/articles";
+	}
+
+	@GetMapping("/edit/{id}")
+	public String edit(Model m, @PathVariable int id) {
+		m.addAttribute("article", articleDao.findById(id));
+		return "/article/addArticle";
+	}
+
+	@PostMapping("/edit/{id}")
+	public String edit(@ModelAttribute Article article) {
+		articleDao.update(article);
+		return "redirect:/articles";
+	}
+
+}
